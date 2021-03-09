1e70a8b930b484fbd049ef3d19a0f3b2e77e5428
jmulford-bandwidth <jmulford@bandwidth.com>
2021-03-09 14:12:12 -0500

    cleanup file

--- lib/bandwidth/web_rtc_lib/web_rtc.rb
+++ lib/bandwidth/web_rtc_lib/web_rtc.rb
@@ -16,7 +16,7 @@ require_relative 'web_rtc/models/device_api_version_enum.rb'
 require_relative 'web_rtc/models/publish_permission_enum.rb'
 
 # Exceptions
-require_relative 'web_rtc/exceptions/error_error_exception.rb'
+require_relative 'web_rtc/exceptions/error_exception.rb'
 # Controllers
 require_relative 'web_rtc/controllers/base_controller.rb'
 require_relative 'web_rtc/controllers/api_controller.rb'
