b730c2344b2fc226443e4035eb79ba7b1dc29369
Jacob Mulford <39915377+jmulford-bw@users.noreply.github.com>
2021-06-21 14:00:49 -0400

    Update test_integration.rb

--- test/integration/test_integration.rb
+++ test/integration/test_integration.rb
@@ -578,6 +578,9 @@ class IntegrationTest < Test::Unit::TestCase
         assert(create_response.data.request_id.length > 0, "request_id value not set")
 
         request_id = create_response.data.request_id
+        
+        puts request_id
+        
         get_response = @bandwidth_client.phone_number_lookup_client.client.get_tn_lookup_result(ACCOUNT_ID, request_id)
         assert(get_response.data.status.length > 0, "status value not set")
     end
