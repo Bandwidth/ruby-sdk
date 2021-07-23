ec7488c45dc67b1c1de014d11b5c1f6eebbe8e95
Audrey Copeland <acopeland@bandwidth.com>
2021-07-23 08:58:22 -0400

    EDGE-630 Add voice call to generate_transfer_bxml_verb

--- lib/bandwidth/web_rtc_lib/utils/web_rtc_transfer.rb
+++ lib/bandwidth/web_rtc_lib/utils/web_rtc_transfer.rb
@@ -6,12 +6,12 @@
 
 module Bandwidth
   module WebRtc
-    def generate_bxml(device_token, sip_uri="sip:sipx.webrtc.bandwidth.com:5060")
-        return '<?xml version="1.0" encoding="UTF-8"?><Response>' + generate_transfer_bxml_verb(device_token, sip_uri) + '</Response>'
+    def generate_bxml(device_token, voice_call_id, sip_uri="sip:sipx.webrtc.bandwidth.com:5060")
+        return '<?xml version="1.0" encoding="UTF-8"?><Response>' + generate_transfer_bxml_verb(device_token, voice_call_id, sip_uri) + '</Response>'
     end
 
-    def generate_transfer_bxml_verb(device_token, sip_uri="sip:sipx.webrtc.bandwidth.com:5060")
-        return '<Transfer><SipUri uui="%s;encoding=jwt">%s</SipUri></Transfer>' % [device_token, sip_uri]
+    def generate_transfer_bxml_verb(device_token, voice_call_id, sip_uri="sip:sipx.webrtc.bandwidth.com:5060")
+        return '<Transfer><SipUri uui="%s;encoding=base64,%s;encoding=jwt">%s</SipUri></Transfer>' % [device_token, voice_call_id, sip_uri]
     end
   end
 end
