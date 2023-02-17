069e89a0b714faf4028f7f564c06b43ff26acb07
ckoegel <ckoegel1006@gmail.com>
2023-02-17 17:09:05 -0500

    switch to 1 set of 4xx tests

--- spec/integration/messages_api_integration_spec.rb
+++ spec/integration/messages_api_integration_spec.rb
@@ -36,7 +36,26 @@ describe 'MessagesApi Integration Tests' do
         expect(response[DATA].media).to eq(body.media)
         expect(response[DATA].text).to eq(message_text)
     end
+  end
 
+  # List Messages
+  describe 'list_messages' do
+    it 'lists messages' do
+      get_opts = {
+        :source_tn => BW_NUMBER,
+        :message_direction => "OUTBOUND"
+      }
+      response = @api_instance_msg.list_messages_with_http_info(BW_ACCOUNT_ID, get_opts)
+
+      expect(response[CODE]).to eq(200)
+      expect(response[DATA].messages[0].account_id).to eq(BW_ACCOUNT_ID)
+      expect(response[DATA].messages[0].message_direction).to eq("OUTBOUND")
+      expect(response[DATA].messages[0].source_tn).to eq(BW_NUMBER)
+    end
+  end
+
+  # HTTP 4XX Errors
+  describe 'http error' do
     it 'causes a 400 error' do
       body = Bandwidth::MessageRequest.new(
         application_id: BW_MESSAGING_APPLICATION_ID,
@@ -68,57 +87,6 @@ describe 'MessagesApi Integration Tests' do
       
       expect {
         $response = @api_instance_msg.create_message_with_http_info(BW_ACCOUNT_ID, body)
-      }.to raise_error { |e|
-        expect(e).to be_a(Bandwidth::ApiError)
-        expect(e.code).to eq(401)
-      }
-    end
-  end
-
-  # List Messages
-  describe 'list_messages' do
-    it 'lists messages' do
-      get_opts = {
-        :source_tn => BW_NUMBER,
-        :message_direction => "OUTBOUND"
-      }
-      response = @api_instance_msg.list_messages_with_http_info(BW_ACCOUNT_ID, get_opts)
-
-      expect(response[CODE]).to eq(200)
-      expect(response[DATA].messages[0].account_id).to eq(BW_ACCOUNT_ID)
-      expect(response[DATA].messages[0].message_direction).to eq("OUTBOUND")
-      expect(response[DATA].messages[0].source_tn).to eq(BW_NUMBER)
-    end
-
-    it 'causes a 400 error' do
-      get_opts = {
-        :source_tn => BW_NUMBER,
-        :message_direction => "OUTBOUND",
-        :page_token => "gdEewhcJLQRB5"
-      }
-
-      expect {
-        response = @api_instance_msg.list_messages_with_http_info(BW_ACCOUNT_ID, get_opts)
-      }.to raise_error { |e|
-        expect(e).to be_a(Bandwidth::ApiError)
-        expect(e.code).to eq(400)
-      }
-    end
-
-    it 'causes a 401 error' do
-      Bandwidth.configure do |config|
-        config.username = 'bad_username'
-        config.password = 'bad_password'
-      end
-
-      get_opts = {
-        :source_tn => BW_NUMBER,
-        :message_direction => "OUTBOUND",
-        :page_token => "gdEewhcJLQRB5"
-      }
-
-      expect {
-        response = @api_instance_msg.list_messages_with_http_info(BW_ACCOUNT_ID, get_opts)
       }.to raise_error { |e|
         expect(e).to be_a(Bandwidth::ApiError)
         expect(e.code).to eq(401)
