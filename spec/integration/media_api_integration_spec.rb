7b07da6f57263be05ea99e12c136395c6bba5a1b
ckoegel <ckoegel1006@gmail.com>
2023-02-20 11:04:30 -0500

    upload and list tests

--- spec/integration/media_api_integration_spec.rb
+++ spec/integration/media_api_integration_spec.rb
@@ -8,19 +8,40 @@ describe 'MediaApi Integration Tests' do
       config.username = BW_USERNAME
       config.password = BW_PASSWORD
     end
-    @api_instance = Bandwidth::MediaApi.new
+    @api_instance_media = Bandwidth::MediaApi.new
   end
 
   after do
     # run after each test
   end
 
-  # Delete Media
-  describe 'delete_media test' do
-    it 'should work' do
-      # assertion here. ref: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
+  # Upload Media
+  describe 'upload_media' do
+    it 'uploads binary media' do
+      media_name = 'ruby_media' + SecureRandom.uuid
+      media_data = '123456'
+
+      response = @api_instance_media.upload_media_with_http_info(BW_ACCOUNT_ID, media_name, media_data)
+      expect(response[CODE]).to eq(204)
+    end
+
+    it 'uploads media file' do
+      media_name = 'ruby_media' + SecureRandom.uuid
+      media_file = File.open("spec/fixtures/ruby_cat.jpeg")
+
+      response = @api_instance_media.upload_media_with_http_info(BW_ACCOUNT_ID, media_name, media_file)
+      expect(response[CODE]).to eq(204)
     end
   end
+  
+    # List Media
+    describe 'list_media' do
+      it 'lists media' do
+        response = @api_instance_media.list_media_with_http_info(BW_ACCOUNT_ID)
+        expect(response[CODE]).to eq(200)
+        expect(response[DATA][0].content_length).to be > 0
+      end
+    end
 
   # Get Media
   describe 'get_media test' do
@@ -29,15 +50,8 @@ describe 'MediaApi Integration Tests' do
     end
   end
 
-  # List Media
-  describe 'list_media test' do
-    it 'should work' do
-      # assertion here. ref: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
-    end
-  end
-
-  # Upload Media
-  describe 'upload_media test' do
+  # Delete Media
+  describe 'delete_media test' do
     it 'should work' do
       # assertion here. ref: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
     end
