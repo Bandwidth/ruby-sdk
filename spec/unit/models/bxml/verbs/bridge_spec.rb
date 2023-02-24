3ff722cde473e2b0817d26e82f126fdf5f53c974
ckoegel <ckoegel1006@gmail.com>
2023-02-24 16:13:16 -0500

    all attributes in bridge test

--- spec/unit/models/bxml/verbs/bridge_spec.rb
+++ spec/unit/models/bxml/verbs/bridge_spec.rb
@@ -2,7 +2,43 @@ require 'spec_helper'
 
 # Unit tests for Bandwidth::Bxml::Bridge
 describe 'Bandwidth::Bxml::Bridge' do
-  let(:instance) { Bandwidth::Bxml::Bridge.new('+19198675309', {bridge_complete_url:  'https://example.com', tag: 'ruby bridge init tag'}) }
+  let(:initial_attributes) {
+    {
+      bridge_complete_url: 'https://initial.com',
+      bridge_complete_method: 'POST',
+      bridge_complete_fallback_url: 'https://initial.com',
+      bridge_complete_fallback_method: 'POST',
+      bridge_target_complete_url: 'https://initial.com',
+      bridge_target_complete_method: 'POST',
+      bridge_target_complete_fallback_url: 'https://initial.com',
+      bridge_target_complete_fallback_method: 'POST',
+      username: 'initial_username',
+      password: 'initial_password',
+      fallback_username: 'initial_fallback_username',
+      fallback_password: 'initial_fallback_password',
+      tag: 'initial_tag'
+    }
+  }
+
+  let(:new_attributes) {
+    {
+      bridge_complete_url: 'https://new.com',
+      bridge_complete_method: 'GET',
+      bridge_complete_fallback_url: 'https://new.com',
+      bridge_complete_fallback_method: 'GET',
+      bridge_target_complete_url: 'https://new.com',
+      bridge_target_complete_method: 'GET',
+      bridge_target_complete_fallback_url: 'https://new.com',
+      bridge_target_complete_fallback_method: 'GET',
+      username: 'new_username',
+      password: 'new_password',
+      fallback_username: 'new_fallback_username',
+      fallback_password: 'new_fallback_password',
+      tag: 'new_tag'
+    }
+  }
+
+  let(:instance) { Bandwidth::Bxml::Bridge.new('+19198675309', initial_attributes) }
 
   describe 'test an instance of Bridge' do
     it 'validates instance of Bridge' do
@@ -10,27 +46,15 @@ describe 'Bandwidth::Bxml::Bridge' do
       expect(instance).to be_a(Bandwidth::Bxml::Verb)
     end
 
-    it 'test the to_bxml method of the Bridge instance' do
-      expected = "\n<Bridge bridgeCompleteUrl=\"https://example.com\" tag=\"ruby bridge init tag\">+19198675309</Bridge>\n"
+    it 'tests the to_bxml method of the Bridge instance' do
+      expected = "\n<Bridge bridgeCompleteUrl=\"https://initial.com\" bridgeCompleteMethod=\"POST\" bridgeCompleteFallbackUrl=\"https://initial.com\" bridgeCompleteFallbackMethod=\"POST\" bridgeTargetCompleteUrl=\"https://initial.com\" bridgeTargetCompleteMethod=\"POST\" bridgeTargetCompleteFallbackUrl=\"https://initial.com\" bridgeTargetCompleteFallbackMethod=\"POST\" username=\"initial_username\" password=\"initial_password\" fallbackUsername=\"initial_fallback_username\" fallbackPassword=\"initial_fallback_password\" tag=\"initial_tag\">+19198675309</Bridge>\n"
       expect(instance.to_bxml).to eq(expected)
     end
 
-    it 'test the set_attributes method of the Bridge instance' do
-      attributes = {'bridge_complete_method' => 'POST', 'tag' => 'ruby bridge set tag'}
-      instance.set_attributes(attributes)
-      expected = "\n<Bridge bridgeCompleteMethod=\"POST\" tag=\"ruby bridge set tag\">+19198675309</Bridge>\n"
+    it 'tests the set_attributes method of the Bridge instance' do
+      instance.set_attributes(new_attributes)
+      expected = "\n<Bridge bridgeCompleteUrl=\"https://new.com\" bridgeCompleteMethod=\"GET\" bridgeCompleteFallbackUrl=\"https://new.com\" bridgeCompleteFallbackMethod=\"GET\" bridgeTargetCompleteUrl=\"https://new.com\" bridgeTargetCompleteMethod=\"GET\" bridgeTargetCompleteFallbackUrl=\"https://new.com\" bridgeTargetCompleteFallbackMethod=\"GET\" username=\"new_username\" password=\"new_password\" fallbackUsername=\"new_fallback_username\" fallbackPassword=\"new_fallback_password\" tag=\"new_tag\">+19198675309</Bridge>\n"
       expect(instance.to_bxml).to eq(expected)
-    end
-
-    it 'test invalid attribute' do
-      attributes = {'invalid_attribute' => 'invalid'}
-      instance.set_attributes(attributes)
-
-      expect {
-        instance.generate_xml
-      }.to raise_error { |e|
-        expect(e).to be_a(NoMethodError)
-      }
     end
   end
 end
