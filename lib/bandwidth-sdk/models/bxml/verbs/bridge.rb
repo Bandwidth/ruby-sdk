c2b9d95bcd584a78253ed150b74937814de3e032
ckoegel <ckoegel1006@gmail.com>
2023-02-24 13:36:37 -0500

    update bridge and add unit test

--- lib/bandwidth-sdk/models/bxml/verbs/bridge.rb
+++ lib/bandwidth-sdk/models/bxml/verbs/bridge.rb
@@ -8,21 +8,21 @@ module Bandwidth
       def initialize(target_call, attributes = {})
         super("Bridge", target_call, attributes)
         
-        @attribute_map = [
-          'bridge_complete_url',                    # Optional [String]: URL to send the Bridge Complete event to and request new BXML. If this attribute is specified, then Verbs following the <Bridge> verb will be ignored and the BXML returned in this webhook is executed on the call. If this attribute is not specified then no webhook will be sent, and execution of the verbs following the <Bridge> verb continues. May be a relative URL. Defaults to None.
-          'bridge_complete_method',                 # Optional [String]: The HTTP method to use for the request to bridgeCompleteUrl. GET or POST. Default value is POST.
-          'bridge_complete_fallback_url',           # Optional [String]: A fallback url which, if provided, will be used to retry the Bridge Complete webhook delivery in case bridgeCompleteUrl fails to respond. Defaults to None.
-          'bridge_complete_fallback_method',        # Optional [String]: The HTTP method to use to deliver the Bridge Complete webhook to bridgeCompleteFallbackUrl. GET or POST. Default value is POST.
-          'bridge_target_complete_url',             # Optional [String]:URL to send the Bridge Target Complete event to and request new BXML. If this attribute is specified, then the BXML returned in this webhook is executed on the target call. If this attribute is not specified then no webhook will be sent, and the target call will be hung up. May be a relative URL. Defaults to None.
-          'bridge_target_complete_method',          # Optional [String]: The HTTP method to use for the request to bridgeTargetCompleteUrl. GET or POST. Default value is POST.
-          'bridge_target_complete_fallback_url',    # Optional [String]: A fallback url which, if provided, will be used to retry the Bridge Target Complete webhook delivery in case bridgeTargetCompleteUrl fails to respond. Defaults to None.
-          'bridge_target_complete_fallback_method', # Optional [String]: The HTTP method to use to deliver the Bridge Target Complete webhook to bridgeTargetCompleteFallbackUrl. GET or POST. Default value is POST.
-          'username',                               # Optional [String]: The username to send in the HTTP request to bridgeCompleteUrl and to bridgeTargetCompleteUrl. Defaults to None.
-          'password',                               # Optional [String]: The password to send in the HTTP request to bridgeCompleteUrl and to bridgeTargetCompleteUrl. Defaults to None.
-          'fallback_username',                      # Optional [String]: The username to send in the HTTP request to bridgeCompleteFallbackUrl and to bridgeTargetCompleteFallbackUrl. Defaults to None.
-          'fallback_password',                      # Optional [String]: The password to send in the HTTP request to bridgeCompleteFallbackUrl and to bridgeTargetCompleteFallbackUrl. Defaults to None.
-          'tag',                                    # Optional [String]: A custom string that will be sent with the bridgeComplete webhook and all future webhooks of the call unless overwritten by a future tag attribute or <Tag> verb, or cleared. May be cleared by setting tag="". Max length 256 characters. Defaults to None.
-        ]
+        @attribute_map = {
+          bridge_complete_url: 'bridgeCompleteUrl',                                     # Optional [String]: URL to send the Bridge Complete event to and request new BXML. If this attribute is specified, then Verbs following the <Bridge> verb will be ignored and the BXML returned in this webhook is executed on the call. If this attribute is not specified then no webhook will be sent, and execution of the verbs following the <Bridge> verb continues. May be a relative URL. Defaults to None.
+          bridge_complete_method: 'bridgeCompleteMethod',                               # Optional [String]: The HTTP method to use for the request to bridgeCompleteUrl. GET or POST. Default value is POST.
+          bridge_complete_fallback_url: 'bridgeCompleteFallbackUrl',                    # Optional [String]: A fallback url which, if provided, will be used to retry the Bridge Complete webhook delivery in case bridgeCompleteUrl fails to respond. Defaults to None.
+          bridge_complete_fallback_method: 'bridgeCompleteFallbackMethod',              # Optional [String]: The HTTP method to use to deliver the Bridge Complete webhook to bridgeCompleteFallbackUrl. GET or POST. Default value is POST.
+          bridge_target_complete_url: 'bridgeTargetCompleteUrl',                        # Optional [String]:URL to send the Bridge Target Complete event to and request new BXML. If this attribute is specified, then the BXML returned in this webhook is executed on the target call. If this attribute is not specified then no webhook will be sent, and the target call will be hung up. May be a relative URL. Defaults to None.
+          bridge_target_complete_method: 'bridgeTargetCompleteMethod',                  # Optional [String]: The HTTP method to use for the request to bridgeTargetCompleteUrl. GET or POST. Default value is POST.
+          bridge_target_complete_fallback_url: 'bridgeTargetCompleteFallbackUrl',       # Optional [String]: A fallback url which, if provided, will be used to retry the Bridge Target Complete webhook delivery in case bridgeTargetCompleteUrl fails to respond. Defaults to None.
+          bridge_target_complete_fallback_method: 'bridgeTargetCompleteFallbackMethod', # Optional [String]: The HTTP method to use to deliver the Bridge Target Complete webhook to bridgeTargetCompleteFallbackUrl. GET or POST. Default value is POST.
+          username: 'username',                                                         # Optional [String]: The username to send in the HTTP request to bridgeCompleteUrl and to bridgeTargetCompleteUrl. Defaults to None.
+          password: 'password',                                                         # Optional [String]: The password to send in the HTTP request to bridgeCompleteUrl and to bridgeTargetCompleteUrl. Defaults to None.
+          fallback_username: 'fallbackUsername',                                        # Optional [String]: The username to send in the HTTP request to bridgeCompleteFallbackUrl and to bridgeTargetCompleteFallbackUrl. Defaults to None.
+          fallback_password: 'fallbackPassword',                                        # Optional [String]: The password to send in the HTTP request to bridgeCompleteFallbackUrl and to bridgeTargetCompleteFallbackUrl. Defaults to None.
+          tag: 'tag',                                                                   # Optional [String]: A custom string that will be sent with the bridgeComplete webhook and all future webhooks of the call unless overwritten by a future tag attribute or <Tag> verb, or cleared. May be cleared by setting tag="". Max length 256 characters. Defaults to None.
+        }
       end
     end
   end
---
+++ spec/unit/models/bxml/verbs/bridge_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+# Unit tests for Bandwidth::Bxml::Bridge
+describe 'Bandwidth::Bxml::Bridge' do
+  let(:instance) { Bandwidth::Bxml::Bridge.new('+19198675309', {bridge_complete_url:  'https://example.com', tag: 'ruby bridge init tag'}) }
+
+  describe 'test an instance of Bridge' do
+    it 'validates insatnce of Bridge' do
+      expect(instance).to be_instance_of(Bandwidth::Bxml::Bridge)
+    end
+
+    it 'test the to_bxml method of the Bridge instance' do
+      expected = "\n<Bridge bridgeCompleteUrl=\"https://example.com\" tag=\"ruby bridge init tag\">+19198675309</Bridge>\n"
+      expect(instance.to_bxml).to eq(expected)
+    end
+
+    it 'test the set_attributes method of the Bridge instance' do
+      attributes = {'bridge_complete_method' => 'POST', 'tag' => 'ruby bridge set tag'}
+      instance.set_attributes(attributes)
+      expected = "\n<Bridge bridgeCompleteMethod=\"POST\" tag=\"ruby bridge set tag\">+19198675309</Bridge>\n"
+      expect(instance.to_bxml).to eq(expected)
+    end
+
+    it 'test invalid attribute' do
+      attributes = {'invalid_attribute' => 'invalid'}
+      instance.set_attributes(attributes)
+
+      expect {
+        instance.generate_xml
+      }.to raise_error { |e|
+        expect(e).to be_a(NoMethodError)
+      }
+    end
+  end
+end
