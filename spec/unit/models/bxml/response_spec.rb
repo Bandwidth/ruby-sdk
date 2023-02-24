7dec8527c670275dce3e1c0189528d2e9272dc5c
ckoegel <ckoegel1006@gmail.com>
2023-02-24 11:56:12 -0500

    add unit tests for Bxml and Response

---
+++ spec/unit/models/bxml/response_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+# Unit tests for Bandwidth::Bxml::Response
+describe 'Bandwidth::Bxml::Response' do
+  let(:instance) { Bandwidth::Bxml::Response.new }
+
+  describe 'test an instance of Response' do
+    it 'validates insatnce of Response' do
+      expect(instance).to be_instance_of(Bandwidth::Bxml::Response)
+    end
+
+    it 'test the to_bxml method of the Response instance' do
+      expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response/>\n"
+      expect(instance.to_bxml).to eq(expected)
+    end
+  end
+end
