6d17561dcc8bf5668b2ef3d52b2890ead77bda19
ckoegel <ckoegel1006@gmail.com>
2023-02-28 09:55:58 -0500

    test decimals in ring verb

--- spec/unit/models/bxml/verbs/ring_spec.rb
+++ spec/unit/models/bxml/verbs/ring_spec.rb
@@ -4,14 +4,14 @@ require 'spec_helper'
 describe 'Bandwidth::Bxml::Ring' do
   let(:initial_attributes) {
     {
-      duration: 5,
+      duration: 5.0,
       answer_call: true
     }
   }
 
   let(:new_attributes) {
     {
-      duration: 10,
+      duration: 10.0,
       answer_call: false
     }
   }
@@ -25,13 +25,13 @@ describe 'Bandwidth::Bxml::Ring' do
     end
 
     it 'tests the to_bxml method of the Ring instance' do
-      expected = "\n<Ring duration=\"5\" answerCall=\"true\"/>\n"
+      expected = "\n<Ring duration=\"5.0\" answerCall=\"true\"/>\n"
       expect(instance.to_bxml).to eq(expected)
     end
 
     it 'tests the set_attributes method of the Ring instance' do
       instance.set_attributes(new_attributes)
-      expected = "\n<Ring duration=\"10\" answerCall=\"false\"/>\n"
+      expected = "\n<Ring duration=\"10.0\" answerCall=\"false\"/>\n"
       expect(instance.to_bxml).to eq(expected)
     end
   end
