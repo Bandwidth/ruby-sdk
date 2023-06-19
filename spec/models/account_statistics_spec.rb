830e24ffc7f7a33ae0a953572f1b598ed4110b09
ckoegel <ckoegel1006@gmail.com>
2023-06-19 16:38:19 -0400

    answer callback tests and improve

--- spec/models/account_statistics_spec.rb
+++ spec/models/account_statistics_spec.rb
@@ -1,8 +1,10 @@
 # Unit tests for Bandwidth::AccountStatistics
 describe Bandwidth::AccountStatistics do
   let(:account_statistics_default) { Bandwidth::AccountStatistics.new }
-  let(:account_statistics_values) { Bandwidth::AccountStatistics.new({ current_call_queue_size: 10, max_call_queue_size: 100 }) }
-  let(:account_statistics_from_hash) { Bandwidth::AccountStatistics.build_from_hash({ currentCallQueueSize: 10, maxCallQueueSize: 100 }) }
+  let(:account_statistics_values) { Bandwidth::AccountStatistics.new({
+    current_call_queue_size: 10,
+    max_call_queue_size: 100
+  }) }
 
   describe '#initialize' do
     it 'validates instance of AccountStatistics initialized with attributes passed in' do
@@ -28,20 +30,12 @@ describe Bandwidth::AccountStatistics do
     end
   end
 
-  describe 'attributes' do   
-    it 'can set and retrieve attributes' do
-      account_statistics_default.current_call_queue_size = 10
-      account_statistics_default.max_call_queue_size = 100
-      
-      expect(account_statistics_values.current_call_queue_size).to eq(10)
-      expect(account_statistics_values.max_call_queue_size).to eq(100)
-      expect(account_statistics_default.current_call_queue_size).to eq(10)
-      expect(account_statistics_default.max_call_queue_size).to eq(100)
-    end
-  end
-
   describe '#build_from_hash' do
     it 'validates instance of AccountStatistics created by the build_from_hash method' do
+      account_statistics_from_hash = Bandwidth::AccountStatistics.build_from_hash({
+        currentCallQueueSize: 10,
+        maxCallQueueSize: 100
+      })
       expect(account_statistics_from_hash).to be_instance_of(Bandwidth::AccountStatistics)
       expect(account_statistics_from_hash.current_call_queue_size).to eq(10)
       expect(account_statistics_from_hash.max_call_queue_size).to eq(100)
@@ -75,14 +69,17 @@ describe Bandwidth::AccountStatistics do
 
   describe '#eq? #==' do
     it 'returns true/false when comparing objects' do
-      expect(account_statistics_from_hash.eql?(account_statistics_values)).to be(true)
-      expect(account_statistics_default.eql?(account_statistics_values)).to be(false)
+      expect(account_statistics_default.eql?(Bandwidth::AccountStatistics.new)).to be true
+      expect(account_statistics_default.eql?(account_statistics_values)).to be false
     end
   end
 
   describe '#to_body #to_hash' do
     it 'returns a hash representation of the object' do
-      expect(account_statistics_values.to_body).to eq({ currentCallQueueSize: 10, maxCallQueueSize: 100 })
+      expect(account_statistics_values.to_body).to eq({
+        currentCallQueueSize: 10,
+        maxCallQueueSize: 100
+      })
     end
   end
 end
