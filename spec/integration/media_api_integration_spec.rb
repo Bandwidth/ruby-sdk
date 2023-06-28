6663ae7b0b4ea8c6f27a4e05e6cc23da20ba20ad
ckoegel <ckoegel1006@gmail.com>
2023-06-28 15:34:26 -0400

    media

--- spec/integration/media_api_integration_spec.rb
+++ spec/integration/media_api_integration_spec.rb
@@ -4,56 +4,63 @@ require 'json'
 # Integration Tests for Bandwidth::MediaApi
 describe 'MediaApi Integration Tests' do
   before(:all) do
+    WebMock.allow_net_connect!
     Bandwidth.configure do |config|
       config.username = BW_USERNAME
       config.password = BW_PASSWORD
       config.return_binary_data = true
     end
     @api_instance_media = Bandwidth::MediaApi.new
+    
+    # media info
     @binary_media_name = 'ruby_binary_media' + SecureRandom.uuid
     @binary_media_data = '123456'
     @media_file_name = 'ruby_media_file' + SecureRandom.uuid
-    @media_file_data = File.open("spec/fixtures/ruby_cat.jpeg").read
+    @media_file_data = File.open('spec/fixtures/ruby_cat.jpeg').read
     @media_file_md5 = Digest::MD5.hexdigest(@media_file_data)
+  end
+
+  after(:all) do
+    WebMock.disable_net_connect!
   end
 
   # Upload Media
   describe 'upload_media' do
     it 'uploads binary media' do
-      response = @api_instance_media.upload_media_with_http_info(BW_ACCOUNT_ID, @binary_media_name, @binary_media_data)
-      expect(response[CODE]).to eq(204)
+      data, status_code, headers = @api_instance_media.upload_media_with_http_info(BW_ACCOUNT_ID, @binary_media_name, @binary_media_data)
+      expect(status_code).to eq(204)
     end
 
     it 'uploads media file' do
-      response = @api_instance_media.upload_media_with_http_info(BW_ACCOUNT_ID, @media_file_name, @media_file_data)
-      expect(response[CODE]).to eq(204)
+      data, status_code, headers = @api_instance_media.upload_media_with_http_info(BW_ACCOUNT_ID, @media_file_name, @media_file_data)
+      expect(status_code).to eq(204)
     end
   end
   
   # List Media
   describe 'list_media' do
     it 'lists media' do
-      response = @api_instance_media.list_media_with_http_info(BW_ACCOUNT_ID)
-      expect(response[CODE]).to eq(200)
-      expect(response[DATA][0]).to be_instance_of(Bandwidth::Media)
-      expect(response[DATA][0].content).to be_instance_of(String)
-      expect(response[DATA][0].content_length).to be > 0
+      data, status_code, headers = @api_instance_media.list_media_with_http_info(BW_ACCOUNT_ID)
+      expect(status_code).to eq(200)
+      expect(data[0]).to be_instance_of(Bandwidth::Media)
+      expect(data[0].content).to be_instance_of(String)
+      expect(data[0].content_length).to be > 0
     end
   end
 
   # Get Media
   describe 'get_media' do
     it 'gets uploaded binary media' do
-      response = @api_instance_media.get_media_with_http_info(BW_ACCOUNT_ID, @binary_media_name)
-      expect(response[CODE]).to eq(200)
-      expect(response[DATA]).to eq(@binary_media_data)
+      data, status_code, headers = @api_instance_media.get_media_with_http_info(BW_ACCOUNT_ID, @binary_media_name)
+      expect(status_code).to eq(200)
+      expect(data).to eq(@binary_media_data)
     end
 
     it 'gets uploaded media file' do
-      response = @api_instance_media.get_media_with_http_info(BW_ACCOUNT_ID, @media_file_name)
-      response_md5 = Digest::MD5.hexdigest(response[DATA])
+      data, status_code, headers = @api_instance_media.get_media_with_http_info(BW_ACCOUNT_ID, @media_file_name)
+      response_md5 = Digest::MD5.hexdigest(data)
 
-      expect(response[CODE]).to eq(200)
+      expect(status_code).to eq(200)
       expect(response_md5).to eq(@media_file_md5)
     end
   end
@@ -61,13 +68,13 @@ describe 'MediaApi Integration Tests' do
   # Delete Media
   describe 'delete_media' do
     it 'deletes the binary media' do
-      response = @api_instance_media.delete_media_with_http_info(BW_ACCOUNT_ID, @binary_media_name)
-      expect(response[CODE]).to eq(204)
+      data, status_code, headers = @api_instance_media.delete_media_with_http_info(BW_ACCOUNT_ID, @binary_media_name)
+      expect(status_code).to eq(204)
     end
 
     it 'deletes the media file' do
-      response = @api_instance_media.delete_media_with_http_info(BW_ACCOUNT_ID, @media_file_name)
-      expect(response[CODE]).to eq(204)
+      data, status_code, headers = @api_instance_media.delete_media_with_http_info(BW_ACCOUNT_ID, @media_file_name)
+      expect(status_code).to eq(204)
     end
   end
 
@@ -75,7 +82,7 @@ describe 'MediaApi Integration Tests' do
   describe 'http error' do
     it 'causes a 404 error' do
       expect {
-        response = @api_instance_media.get_media_with_http_info(BW_ACCOUNT_ID, "does_not_exist")
+        @api_instance_media.get_media_with_http_info(BW_ACCOUNT_ID, 'does_not_exist')
       }.to raise_error { |e|
         expect(e).to be_instance_of(Bandwidth::ApiError)
         expect(e.code).to eq(404)
@@ -83,4 +90,4 @@ describe 'MediaApi Integration Tests' do
     end
   end
 
-end if false # (`if false` skips this entire block)
+end
