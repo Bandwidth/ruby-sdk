979e334057848b8e696e0fdc6a8572745401f3aa
DX-Bandwidth <dx@bandwidth.com>
2024-05-07 14:06:46 +0000

    Generate SDK with OpenAPI Generator Version 7.0.0

--- lib/bandwidth-sdk/models/page_info.rb
+++ lib/bandwidth-sdk/models/page_info.rb
@@ -62,7 +62,7 @@ module Bandwidth
     # @param [Hash] attributes Model attributes in the form of hash
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, 'The input argument (attributes) must be a hash in `Bandwidth::PageInfo` initialize method'
+        fail ArgumentError, "The input argument (attributes) must be a hash in `Bandwidth::PageInfo` initialize method"
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
@@ -239,5 +239,7 @@ module Bandwidth
         value
       end
     end
+
   end
+
 end
