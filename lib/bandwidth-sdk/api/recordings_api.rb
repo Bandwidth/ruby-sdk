be1124e9d8950b79c37714fe0045792df07fe092
DX-Bandwidth <dx@bandwidth.com>
2024-06-04 14:07:33 +0000

    Clean SDK using Rubocop

--- lib/bandwidth-sdk/api/recordings_api.rb
+++ lib/bandwidth-sdk/api/recordings_api.rb
@@ -667,7 +667,7 @@ module Bandwidth
       # HTTP header 'Content-Type'
       content_type = @api_client.select_header_content_type(['application/json'])
       if !content_type.nil?
-          header_params['Content-Type'] = content_type
+        header_params['Content-Type'] = content_type
       end
 
       # form parameters
@@ -747,7 +747,7 @@ module Bandwidth
       # HTTP header 'Content-Type'
       content_type = @api_client.select_header_content_type(['application/json'])
       if !content_type.nil?
-          header_params['Content-Type'] = content_type
+        header_params['Content-Type'] = content_type
       end
 
       # form parameters
