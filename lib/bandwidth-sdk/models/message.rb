8eda42183df57883c06c06a7d154a90358a6ab0b
DX-Bandwidth <dx@bandwidth.com>
2024-06-13 19:11:50 +0000

    Clean SDK using Rubocop

--- lib/bandwidth-sdk/models/message.rb
+++ lib/bandwidth-sdk/models/message.rb
@@ -127,7 +127,7 @@ module Bandwidth
     # @param [Hash] attributes Model attributes in the form of hash
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `Bandwidth::Message` initialize method"
+        fail ArgumentError, 'The input argument (attributes) must be a hash in `Bandwidth::Message` initialize method'
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
@@ -373,7 +373,5 @@ module Bandwidth
         value
       end
     end
-
   end
-
 end
--- lib/bandwidth-sdk/models/update_call_recording.rb
+++ lib/bandwidth-sdk/models/update_call_recording.rb
@@ -68,7 +68,7 @@ module Bandwidth
     # @param [Hash] attributes Model attributes in the form of hash
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `Bandwidth::UpdateCallRecording` initialize method"
+        fail ArgumentError, 'The input argument (attributes) must be a hash in `Bandwidth::UpdateCallRecording` initialize method'
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
@@ -237,7 +237,5 @@ module Bandwidth
         value
       end
     end
-
   end
-
 end
