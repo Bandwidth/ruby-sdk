fbd9925dafa5fb8cc57670f94dc4ae994a23392a
ckoegel <ckoegel1006@gmail.com>
2024-07-02 10:47:47 -0400

    regenerate and update gemfile

--- lib/bandwidth-sdk/models/messaging_request_error.rb
+++ lib/bandwidth-sdk/models/messaging_request_error.rb
@@ -50,7 +50,7 @@ module Bandwidth
     # @param [Hash] attributes Model attributes in the form of hash
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `Bandwidth::MessagingRequestError` initialize method"
+        fail ArgumentError, 'The input argument (attributes) must be a hash in `Bandwidth::MessagingRequestError` initialize method'
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
@@ -231,7 +231,5 @@ module Bandwidth
         value
       end
     end
-
   end
-
 end
