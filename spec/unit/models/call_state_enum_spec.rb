f1e0b52aa6e2999981e021e7bad9e6fc91c8e6f4
ckoegel <ckoegel1006@gmail.com>
2026-05-18 13:39:20 -0400

    update current tests

--- spec/unit/models/call_state_enum_spec.rb
+++ spec/unit/models/call_state_enum_spec.rb
@@ -1,10 +1,25 @@
 # Unit tests for Bandwidth::CallStateEnum
 describe Bandwidth::CallStateEnum do
-  let(:instance) { Bandwidth::CallStateEnum.new }
+  describe 'constants' do
+    it 'defines ACTIVE' do
+      expect(Bandwidth::CallStateEnum::ACTIVE).to eq('active')
+    end
 
-  describe 'test an instance of CallStateEnum' do
-    it 'creates an instance of CallStateEnum' do
-      expect(instance).to be_instance_of(Bandwidth::CallStateEnum)
+    it 'defines COMPLETED' do
+      expect(Bandwidth::CallStateEnum::COMPLETED).to eq('completed')
+    end
+  end
+
+  describe '.all_vars' do
+    it 'returns every valid enum value' do
+      expect(Bandwidth::CallStateEnum.all_vars).to eq(['active', 'completed'])
+    end
+  end
+
+  describe '.build_from_hash' do
+    it 'returns the value when it matches a valid enum value' do
+      expect(Bandwidth::CallStateEnum.build_from_hash('active')).to eq('active')
+      expect(Bandwidth::CallStateEnum.build_from_hash('completed')).to eq('completed')
     end
 
     it 'raises an error for an invalid enum value' do
